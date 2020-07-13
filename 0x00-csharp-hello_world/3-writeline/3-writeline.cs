44d3fbead110736fbd6ad144a9fee56502868a1e
pizzadelivery01 <50601948+pizzadelivery01@users.noreply.github.com>
2020-07-13 16:07:59 -0400

    indented method

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -4,8 +4,8 @@
     {
         private const string Value = "\"Programming is like building a multilingual puzzle";
             static void Main(string[] args)
-        {
-            Console.WriteLine(Value);
-        }
+            {
+                Console.WriteLine(Value);
+            }
     }
 
