c8ffcc96639398549167f16f1cf91d603689538d
pizzadelivery01 <50601948+pizzadelivery01@users.noreply.github.com>
2020-07-13 16:01:33 -0400

    writeline pushed

---
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    class Program
+    {
+        private const string Value = "\"Programming is like building a multilingual puzzle";
+            static void Main(string[] args)
+        {
+            Console.WriteLine(Value);
+        }
+    }
+
