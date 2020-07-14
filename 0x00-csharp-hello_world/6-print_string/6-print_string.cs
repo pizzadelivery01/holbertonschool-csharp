8e8bc37d622f3bc16dc931162bde50bd03211ed1
jeremy bedolla <jjbedolla@gmail.com>
2020-07-13 22:05:20 -0400

    capitalized String

--- 0x00-csharp-hello_world/6-print_string/6-print_string.cs
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,7 +5,7 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "Holberton School";
-		string triple = string.Join("", Enumeralbe.Repeat(str,3));
+		string triple = String.Join("", Enumeralbe.Repeat(str,3));
         Console.WriteLine({0}, triple);
 		Console.WriteLine({0}, str.Substring(0,9));
     }
