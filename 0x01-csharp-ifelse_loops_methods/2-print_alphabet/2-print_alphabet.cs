0f753deff69e4335c89bbc112a89da19380a8a44
jeremy bedolla <jjbedolla@gmail.com>
2020-07-14 17:38:00 -0400

    added quotes

--- 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -6,7 +6,7 @@ class Program
     static void Main(string[] args)
     {
         char letter = "a";
-        while (letter <= z)
+        while (letter <= "z")
         {
             Console.Write(letter);
             letter++;
