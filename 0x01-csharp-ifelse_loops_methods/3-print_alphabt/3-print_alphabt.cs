48ff7123a89336f33c5e112d0f18ce8d7eb080a0
jeremy bedolla <jjbedolla@gmail.com>
2020-07-14 18:29:19 -0400

    reworked

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 26; i++)
+        {
+            if (i != 4 && i != 16)
+            {
+                Console.Write(Convert.ToChar(i + (int)'a'));
+            }
+        }
+    }
+}
