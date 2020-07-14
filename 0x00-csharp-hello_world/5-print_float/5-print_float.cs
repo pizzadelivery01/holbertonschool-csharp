97d43e9cb5368569cdc7e28fab57fb6c4703b4f0
jeremy bedolla <jjbedolla@gmail.com>
2020-07-13 21:37:27 -0400

    updated print statement

--- 0x00-csharp-hello_world/5-print_float/5-print_float.cs
+++ 0x00-csharp-hello_world/5-print_float/5-print_float.cs
@@ -5,6 +5,6 @@ class Program
 	static void Main(string[] args)
         {
 		float number = 3.14159f;
-		Console.WriteLine("{0.F2}", number);
+		Console.WriteLine("Float: {0.F2}", number);
         }
 }
