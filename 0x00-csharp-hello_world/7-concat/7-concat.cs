e6a8ecdd369fc9a406aafc496e50fad9e296ae16
jeremy bedolla <jjbedolla@gmail.com>
2020-07-13 22:15:22 -0400

    added space for string

--- 0x00-csharp-hello_world/7-concat/7-concat.cs
+++ 0x00-csharp-hello_world/7-concat/7-concat.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		string str1 = "Holberton";
 		string str2 = "School";
-		string str1 = String.Concat(str1,str2);
+		string str1 = string.Concat(str1, " ", str2);
 		Console.WriteLine("Welcome to {0}!", str1);
 	}
 }
