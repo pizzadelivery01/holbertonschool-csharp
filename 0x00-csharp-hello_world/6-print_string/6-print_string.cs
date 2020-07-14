10f9eb96da4c1cbc6026fad3736045b0525336a4
jeremy bedolla <jjbedolla@gmail.com>
2020-07-13 22:07:39 -0400

    old way will not work in .net <4+

--- 0x00-csharp-hello_world/6-print_string/6-print_string.cs
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,8 +5,7 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "Holberton School";
-		string triple = String.Join("", Enumeralbe.Repeat(str,3));
-        Console.WriteLine({0}, triple);
+        Console.WriteLine({0}{0}{0}, str);
 		Console.WriteLine({0}, str.Substring(0,9));
     }
 }
