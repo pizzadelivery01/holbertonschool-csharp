5e054c2c6cd632ba761a74b39ad8276f9bc14416
jeremy bedolla <jjbedolla@gmail.com>
2020-07-14 14:33:45 -0400

    adding last digit

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int last = number % 10;
+        if (last == 0)
+        {
+            Console.WriteLine("The last digit of {0} is {1} and is 0", number, last);
+        }
+        else
+        {
+            if (last > 5)
+            {
+                Console.WriteLine("The last digit of {0} is {1} and greater than 5", number, last);
+            }
+            else
+            {
+                Console.WriteLine("The last digit of {0} is {1} and is less than 6 and not 0", number, last);
+            }
+        }
+    }
+}
