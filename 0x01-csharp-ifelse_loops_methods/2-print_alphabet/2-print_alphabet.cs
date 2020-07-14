4049acf1b0e63229d7d807c9c3fe93a84e1adbf8
jeremy bedolla <jjbedolla@gmail.com>
2020-07-14 17:23:32 -0400

    adding print abc

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    string letter = "a"
+    static void Main(string[] args)
+    {
+        while (letter <= z)
+        {
+            Console.Write(letter);
+            letter++;
+        }
+    }
+}
