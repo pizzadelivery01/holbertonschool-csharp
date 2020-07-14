dca70d66072f3afa5e08643e3752c34bc3c5c7a1
jeremy bedolla <jjbedolla@gmail.com>
2020-07-14 17:13:10 -0400

    added if conditional

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -6,7 +6,7 @@ class Program
     {
         Random rndm = new Random();
         int number = rndm.Next(-10000, 10000);
-        int last = number % 10;
+        int last = number % (10);
         
         if (last == 0)
         {
@@ -14,18 +14,20 @@ class Program
         }
         else
         {
-            if (number < 0)
-            {
-                Console.WriteLine("The last digit of {0} is -{1} and is less than 6 and not 0", number, last);
-            }
-            if (number > 0 && last < 6)
+            if (number < 0 && last != 0)
             {
                 Console.WriteLine("The last digit of {0} is {1} and is less than 6 and not 0", number, last);
             }
-            if (number > 0 && last > 5)
+            else
             {
-                Console.WriteLine("The last digit of {0} is {1} and greater than 5", number, last);
+                if (number > 0 && last < 6)
+                {
+                    Console.WriteLine("The last digit of {0} is {1} and is less than 6 and not 0", number, last);
+                }
+                if (number > 0 && last > 5)
+                {
+                    Console.WriteLine("The last digit of {0} is {1} and greater than 5", number, last);
+                }
             }
-        }
     }
 }
