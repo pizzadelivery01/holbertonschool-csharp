e4562ad51420a980f18e64d0bcc6c8b647486954
jeremy bedolla <jjbedolla@gmail.com>
2020-07-13 22:00:00 -0400

    read project fully

--- 0x00-csharp-hello_world/6-print_string/6-print_string.cs
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,6 +5,8 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "Holberton School";
-        Console.WriteLine(str);
+		string triple = string.join("", Enumeralbe.Repeat(str,3));
+        Console.WriteLine({0}, triple);
+		Console.WriteLine({0}, str.Substring(0,9));
     }
 }
