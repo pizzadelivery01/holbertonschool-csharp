10c8c242f6aad4b77630a6d356a93dfeb6b8b7c1
jeremy bedolla <jjbedolla@gmail.com>
2020-07-14 20:30:02 -0400

    pring double added

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int i =0; i <= 9; i++)
+        {
+            int first = i;
+            for(int j = 0; j <= 9; j++)
+            {
+                int second = j;
+                Console.Write("{0}{1}, ", first, second);
+            }
+        }
+        Console.WriteLine("99");
+    }
+}
