13d3437da54f1a0ff79aeb2ea8d66827d087f1d0
jeremy bedolla <jjbedolla@gmail.com>
2020-07-14 20:15:42 -0400

    changed to 98 from 99

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-       for(int i=0; i <= 99; i++)
+       for(int i=0; i <= 98; i++)
        {
            string hex = i.ToString("x");
            Console.WriteLine("{0} = 0x{1}", i, hex);
