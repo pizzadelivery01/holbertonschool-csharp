693de07d2855a8ba120b297e92a6415264297daa
jeremy bedolla <jjbedolla@gmail.com>
2020-07-14 21:10:33 -0400

    adding diag line

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Line 
+{
+    public static void PrintDiagonal(int length)
+    { 
+        string line = "";
+        if (length > 0) 
+        {
+            for (int i = 1; i <= length; i++)
+            {
+                line = new String(' ', i - 1);
+                Console.WriteLine("{0}\\", line);
+            }
+        }
+        Console.Write("\n");
+    }
+}
