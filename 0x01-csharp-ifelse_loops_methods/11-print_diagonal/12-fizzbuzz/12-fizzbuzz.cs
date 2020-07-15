bf63f1df63025456d526aca9eedf452ff3dba839
jeremy bedolla <jjbedolla@gmail.com>
2020-07-14 21:14:47 -0400

    adding fizzbuzz

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 0;
+        for (i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0)
+                Console.Write("FizzBuzz");
+            else if (i % 5 == 0) {
+                Console.Write("Buzz");
+            }
+            else if (i % 3 == 0) {
+                Console.Write("Fizz");
+            }
+            else
+            {
+                Console.Write(i);
+            }
+            if (i != 100) {
+                Console.Write(" ");
+            }
+            else {
+                Console.Write("\n");
+            }
+        }
+    }
+}
