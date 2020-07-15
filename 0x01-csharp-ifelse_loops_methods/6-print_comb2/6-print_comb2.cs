c25c19ddb84a5a3b559c66cc66fbe6a97e14f0a0
jeremy bedolla <jjbedolla@gmail.com>
2020-07-14 20:53:56 -0400

    added pirnt digits no double

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 89; i++)
+            {
+                string number = i.ToString("00");
+                if(i != 89)
+                {
+                    if(number[0] - '0' < number[1] - '0')
+                    {
+                        Console.Write("{0:00}, ", i);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("{0:00}", i);
+                }
+            }
+        }
+    }
+}
