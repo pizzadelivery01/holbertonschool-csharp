cafc521b5cf4d0870a1bdf3a9079cabb1113f9f6
jeremy bedolla <jjbedolla@gmail.com>
2020-07-14 21:07:24 -0400

    added print line

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Line 
+{
+    public static void PrintLine(int length)
+    { 
+        string line = "";
+        if (length >= 0)
+        {
+            line = new String('_', length);
+        }
+        Console.WriteLine(line);
+    }
+}
