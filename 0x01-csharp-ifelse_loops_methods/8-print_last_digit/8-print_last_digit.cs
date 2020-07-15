5cde6a6a426af7014f483979bc74da7b2e3f4c46
jeremy bedolla <jjbedolla@gmail.com>
2020-07-14 21:02:04 -0400

    adding print last

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Number 
+{
+    public static int PrintLastDigit(int number)
+    {
+        if (number < 0)
+        {
+            number = number * -1;
+        }
+        int last = number % 10;
+        Console.Write(last);
+        return(last);
+    }
+
+}
