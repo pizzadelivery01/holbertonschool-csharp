d922e1ec55137b4132fd0a19906bded7b904b16f
jeremy bedolla <jjbedolla@gmail.com>
2020-07-16 18:16:39 -0400

    adding printt array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    // method that prints an array of int to a given size
+    {
+        if(size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+            throw new OverflowException();
+        }
+        int[] int_array = new int[size];
+        for(int i =0; i < size; i++)
+        {
+            Console.Write("{0}", i);
+            if(i != (size - 1))
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+        return int_array;
+    }
+}
