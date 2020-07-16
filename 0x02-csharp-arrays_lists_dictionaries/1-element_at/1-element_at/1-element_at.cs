fab855c02e745a5e0c472b48990e907c7beb9ce5
jeremy bedolla <jjbedolla@gmail.com>
2020-07-16 19:20:54 -0400

    adding 1-element_at

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at/1-element_at.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    // method that prints an element at given index from an array
+    {
+        if (index < 0 || index > array.Length - 1)
+        {
+            Console.WriteLine("Index out of range");
+            return -1;
+            throw new IndexOutOfRangeException();
+        }
+        return (array[index]);
+    }
+}
