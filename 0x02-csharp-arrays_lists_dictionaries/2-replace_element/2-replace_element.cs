e28afc42e1c5c84031ac1dd401332bb6d0d2d8bb
jeremy bedolla <jjbedolla@gmail.com>
2020-07-16 19:39:05 -0400

    adding 2-replace-element

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    // method that replaces an element of an array at a given index
+    {
+        if (index > array.Length - 1 | index < 0)
+        {
+            Console.WriteLine("Index out of range");
+            return array;
+            throw new IndexOutOfRangeException();
+        }
+        else
+        {
+            array[index] = n;
+            return array;
+        }
+    }
+}
