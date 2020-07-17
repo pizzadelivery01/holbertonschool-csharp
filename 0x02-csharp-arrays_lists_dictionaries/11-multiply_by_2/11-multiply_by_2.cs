d0fa20ba3fe2fe150f34af3de6e2dd23bd3f475a
jeremy bedolla <jjbedolla@gmail.com>
2020-07-16 21:00:36 -0400

    adding 11-multiply_by_2

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        myDict = myDict.ToDictionary(k => k.Key, v => v.Value * 2);
+        return (myDict);
+    }
+}
