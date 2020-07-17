8b7e4405d8c8012ec3a3e41a6cfaed8a7b7fc3cf
jeremy bedolla <jjbedolla@gmail.com>
2020-07-16 20:53:21 -0400

    adding 10-delet_key_value

---
+++ 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
+    // method that deletes a key in a dictionary
+    {
+        myDict.Remove(key);
+        return myDict;
+    }
+}
