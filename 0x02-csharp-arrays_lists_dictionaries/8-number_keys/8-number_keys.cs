7da2066d375aa58eead1e052733e3d5649674995
jeremy bedolla <jjbedolla@gmail.com>
2020-07-16 20:44:52 -0400

    adding 8-number_keys

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    // method that returns the number of keys in a dictionary
+    {
+        int counter = 0;
+        foreach(KeyValuePair<string, string> entry in myDict)
+        {
+            counter += 1;
+        }
+        return (counter);
+    }
+}
