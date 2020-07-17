1754ebe838d48516162a96fd58f021bc02f0dc22
jeremy bedolla <jjbedolla@gmail.com>
2020-07-16 21:16:06 -0400

    adding 14-rectangular_array

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    // method that creates and prints a 5 by 5 two-dimensional array and initialize index [2, 2] to 1 and all other indices to 0
+    {
+        var array = new int[5, 5];
+        array[2, 2] = 1;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{array[i, 0]} {array[i, 1]} {array[i, 2]} {array[i, 3]} {array[i, 4]}");
+        }
+    }
+}
