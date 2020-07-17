443ed841fe0cb897711acf9e4662dc5266153dde
jeremy bedolla <jjbedolla@gmail.com>
2020-07-16 20:14:38 -0400

    adding 4-print_list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class List 
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0) {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+        List<int> mylist = Enumerable.Range(0, size).ToList();
+        Console.WriteLine(string.Format("{0}", string.Join(" ", mylist)));;
+        return (mylist);
+    }
+}
