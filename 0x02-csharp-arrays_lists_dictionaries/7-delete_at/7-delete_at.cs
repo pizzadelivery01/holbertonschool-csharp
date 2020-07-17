602cf8da856621882104c4f7934972f2577fbf89
jeremy bedolla <jjbedolla@gmail.com>
2020-07-16 20:38:46 -0400

    adding 7-delete_at

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static void DeleteAt(List<int> myList, int index)
+    // method that deletes list item at given index
+    {
+        int flag = 0;
+
+        if (index > myList.Count - 1 || index < 0)
+        {
+            Console.WriteLine("Index is out of range");
+            return;
+            throw new IndexOutOfRangeException();
+        }
+        foreach (int item in myList)
+        {
+            if (flag == index)
+            {
+                myList.Remove(myList[index]);
+                break;
+            }
+            flag++;
+        }
+    }
+}
