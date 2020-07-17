0c41e5b7bbb37166661ec4bbb54abfcf83796eca
jeremy bedolla <jjbedolla@gmail.com>
2020-07-16 20:31:36 -0400

    adding 6-divisible_by_2

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List 
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    // returns list of T/F if multiple of 2
+    {
+        int counter = 0;
+        List<bool> mul_of_two = new List<bool>();
+
+        for (counter = 0; counter <= myList.Count - 1; counter++) {
+            mul_of_two.Add((myList[counter] % 2 == 0) ? true : false);
+        }
+        return (mul_of_two);
+    }
+}
