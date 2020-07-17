608379e160e2639605fc163496a5e234138b5ead
jeremy bedolla <jjbedolla@gmail.com>
2020-07-16 20:49:17 -0400

    adding 9-add_key_value

---
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    // method that adds a key and value to a dictionary
+
+    {
+        myDict.Remove(key);
+        myDict.Add(key, value);
+        return myDict;
+    }
+}
