841e909e40e11faddbd9d9c52f44b55b42ee80c9
jeremy bedolla <jjbedolla@gmail.com>
2020-07-16 21:11:09 -0400

    13-best_score

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    // method that returns the key with the biggest integer value in a given dictionary
+    {
+        int max_value = 0;
+        string max_key = "None";
+
+        foreach (KeyValuePair<string, int> item in myList)
+        {
+            if (item.Value >= max_value)
+            {
+                max_value = item.Value;
+                max_key = item.Key;
+            }
+        }
+        return max_key;
+    }
+}
