7d242716f038e47feed43914790183d9dd52988c
jeremy bedolla <jjbedolla@gmail.com>
2020-07-16 21:20:55 -0400

    adding 15-square_matrix

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    // method that returns the square value of all integers of a matrixin new matrix.
+    {
+        int[,] squared = new int[myMatrix.GetLength(0), myMatrix.GetLength(1)];
+
+        for (int i = 0; i < myMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < myMatrix.GetLength(1); j++)
+            {
+                squared[i, j] = myMatrix[i, j] * myMatrix[i, j];
+            }
+        }
+
+        return (squared);
+    }
+}
