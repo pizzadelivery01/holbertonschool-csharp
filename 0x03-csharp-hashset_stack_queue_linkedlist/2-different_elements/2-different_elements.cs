218a86038c44d6456cadec61b9e2fde39f61d6e8
jeremy bedolla <jjbedolla@gmail.com>
2020-07-27 12:23:03 -0400

    adding 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> myList = new List<int>();
+        int i = 0;
+        while(i < list1.Count)
+        {
+            if(list2.Contains(list1[i]))
+            {
+                i++;
+            }
+            else
+            {
+                myList.Add(list1[i]);
+                i++;
+            }
+        }
+        while(i < list2.Count)
+        {
+            if(list1.Contains(list2[i]))
+            {
+                i++;
+            }
+            else
+            {
+                myList.Add(list2[i]);
+                i++;
+            }
+        }
+        return myList;
+    }
+}
