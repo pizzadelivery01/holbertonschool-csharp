3e5f04db6b96fc037f72c105ebac15874c8547f0
jeremy bedolla <jjbedolla@gmail.com>
2020-07-27 15:24:02 -0400

    adding 7 and 8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int i = 0;
+        foreach (int num in myLList)
+        {
+            if (num == value)
+                return (i);
+            i++;
+        }
+        return (-1);
+    }
+}
