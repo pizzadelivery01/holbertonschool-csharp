cc3d002b8bc3b41d9b72675b3614ba252c68d7e0
jeremy bedolla <jjbedolla@gmail.com>
2020-07-27 12:15:15 -0400

    removed namespace

--- 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -1,25 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace _0_unique_add
-{
-    class List
-    {
-        public static int Sum(List<int> myList)
-        {
-            HashSet<int> set = new HashSet<int>();
-            foreach(int number in myList)
-            {
-                set.Add(number);
-            }
-            int size = set.Count;
-            int total = 0;
-            foreach (int item in set)
-            {
-                total = total + item;
-            }
 
-            return total;
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> set = new HashSet<int>();
+        foreach(int number in myList)
+        {
+            set.Add(number);
         }
+        int size = set.Count;
+        int total = 0;
+        foreach (int item in set)
+        {
+            total = total + item;
+        }
+
+        return total;
     }
 }
--- 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -1,26 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace common_elements
+
+class List
 {
-    class List
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
     {
-        public static List<int> CommonElements(List<int> list1, List<int> list2)
+        List<int> myList = new List<int>();
+        int i = 0;
+        while(i < list1.Count)
         {
-            List<int> myList = new List<int>();
-            int i = 0;
-            while(i < list1.Count)
+            if(list2.Contains(list1[i]))
             {
-                if(list2.Contains(list1[i]))
-                {
-                    myList.Add(list1[i]);
-                    i++;
-                }
-                else{
-                    i++;
-                }
+                myList.Add(list1[i]);
+                i++;
             }
-            return myList;
+            else{
+                i++;
+            }
         }
+        return myList;
     }
 }
