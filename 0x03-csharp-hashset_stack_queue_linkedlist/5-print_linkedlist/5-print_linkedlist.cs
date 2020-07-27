6ca2ae9413b310a83d8f5a04ea0bcf0057b2b2c3
jeremy bedolla <jjbedolla@gmail.com>
2020-07-27 15:18:21 -0400

    adding 5 and 6

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+         LinkedList<int> newList = new LinkedList<int>();
+         for( int i = 0; i < size; i++)
+         {
+             newList.AddLast(i);
+             Console.WriteLine(i);
+         }
+         return newList;
+    }
+}
