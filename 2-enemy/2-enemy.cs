8b0d5059499dbbc9fc59c83b35a740ccce355109
jeremy bedolla <jjbedolla@gmail.com>
2020-07-30 17:15:12 -0400

    adding

---
+++ 2-enemy/2-enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    ///  Represents an enemy zombie class
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// health of zombie.
+        /// </summary>
+        public int health {get; set;}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary>
+        /// Sets health of zombie to value
+        /// </summary>
+        /// <param name="value"></param>
+        public Zombie(int value)
+        {
+            if(value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+    }
+}
