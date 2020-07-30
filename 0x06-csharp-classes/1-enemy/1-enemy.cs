e2acaefd334dff419cab82f91a32612e15ebb2d2
jeremy bedolla <jjbedolla@gmail.com>
2020-07-30 16:59:30 -0400

    adding

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    ///  Represents an enemy zombie class
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Gets or sets health of zombie.
+        /// </summary>
+        public int health {get; set;}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
