745e0ec4faab2310768c4a6bb6b1639dc137f705
jeremy bedolla <jjbedolla@gmail.com>
2020-08-18 12:50:47 -0400

    threading update

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Threading;
+using System.Threading.Tasks;
 using System.IO;
 
 
@@ -13,13 +14,34 @@ class ImageProcessor
 	/// <param name="filenames">list of files</param>
     public static void Inverse(string[] filenames)
     {
-        foreach (string file in filenames)
+        Parallel.ForEach(filenames, file =>
 		{
-			Thread t = new Thread(ImageProcessor.invert);
-			t.Start(file);
-		}
+			string name = file.ToString();
+			int width;
+			int height;
+			string extension = "";
+			string filename = Path.GetFileNameWithoutExtension(name);
+
+			Bitmap bmp = new Bitmap(name);
+			width = bmp.Width;
+			height = bmp.Height;
+			extension = name.Substring(name.LastIndexOf("."));
+
+			for (int x = 0; x < width; ++x)
+			{
+				for ( int y = 0; y < height; ++y)
+				{
+					Color inv = bmp.GetPixel(x,y);
+					int a = inv.A;
+					inv = Color.FromArgb(a, (255 - inv.R), (255 - inv.G), (255 - inv.B));
+					bmp.SetPixel(x, y, inv);
+				}
+			}
+			bmp.Save(filename + "_inverse" + extension);
+			});		
     }
-	/// <summary>
+}
+/**	/// <summary>
 	/// inverts color scheme
 	/// </summary>
 	/// <param name="file">file</param>
@@ -48,4 +70,4 @@ class ImageProcessor
 		}
 		bmp.Save(filename + "_inverse" + extension);
 	}
-}
+}*/
