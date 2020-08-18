c66d7d3ff98ef5ef13ad03302474e48f273d10d9
jeremy bedolla <jjbedolla@gmail.com>
2020-08-18 15:40:14 -0400

    adding bw

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -71,4 +71,49 @@ class ImageProcessor
 			bmp.Save(filename + "_grayscale" + extension);
 		});		
     }
+	/// <summary>
+	/// converts to B and W 
+	/// </summary>
+	/// <param name="filenames">list of images</param>
+	/// <param name="threshold">threshold of b vs W</param>
+	public static void BlackWhite(string[] filenames, double threshold)
+	{
+		Parallel.ForEach(filenames, file =>
+		{
+			string extension = Path.GetExtension(file);
+			string filename = Path.GetFileNameWithoutExtension(file);
+
+			double total;
+
+			Bitmap bmp = new Bitmap(file);
+			Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+			BitmapData data = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+
+			IntPtr ptr = data.Scan0;
+
+			int bytes = Math.Abs(data.Stride) * bmp.Height;
+			byte[] rgbValues = new byte[bytes];
+			Marshal.Copy(ptr, rgbValues, 0, bytes);
+			
+			for(int i = 0; i < rgbValues.Length - 2; i += 3)
+			{
+				total = rgbValues[i] + rgbValues[i + 1] + rgbValues[i + 2];
+				if( total >= threshold)
+				{
+					rgbValues[i] = 255;
+					rgbValues[i + 1] = 255;
+					rgbValues[i + 2] = 255;
+				}
+				else
+				{
+					rgbValues[i] = 0;
+					rgbValues[i + 1] = 0;
+					rgbValues[i + 2] = 0;
+				}
+			}
+			Marshal.Copy(rgbValues, 0, ptr, bytes);
+			bmp.UnlockBits(data);
+			bmp.Save(filename + "_bw" + extension);
+		});	
+	}
 }
