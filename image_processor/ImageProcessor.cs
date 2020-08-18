a426707f3979e40fafa91493974913616252c81d
jeremy bedolla <jjbedolla@gmail.com>
2020-08-18 02:38:01 -0400

    update

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -4,51 +4,52 @@ using System.Drawing;
 using System.Threading;
 using System.IO;
 
-namespace image_processor
+
+class ImageProcessor
 {
-    class ImageProcessor
+	/// <summary>
+	/// invert color on batch of images given using multi-threading
+	/// </summary>
+	/// <param name="filenames">list of files</param>
+    public static void Inverse(string[] filenames)
     {
-		/// <summary>
-		/// invert color on batch of images given using multi-threading
-		/// </summary>
-		/// <param name="filenames"></param>
-        public static void Inverse(string[] filenames)
-        {
-            foreach (string file in filenames)
-			{
-				Thread t = new Thread(ImageProcessor.invert);
-				t.Start(file);
-			}
-        }
-
-		public static void invert(object file)
+        foreach (string file in filenames)
 		{
-			string name = file.ToString();
-			int width;
-			int height;
-			string extension = "";
-			string filename = "";
-
-			Bitmap bmp = new Bitmap(name);
-			width = bmp.Width;
-			height = bmp.Height;
-			extension = name.Substring(name.LastIndexOf("."));
-			filename = name.Substring(name.LastIndexOf("/") + 1, name.Length - extension.Length);
-
-			for (int x = 0; x < width; ++x)
-			{
-				for ( int y = 0; y < height; ++y)
-				{
-					Color color = bmp.GetPixel(x,y);
-
-					int r = 255 - color.R;
-					int g = 255 - color.G;
-					int b = 255 - color.B;
-					int a = color.A;
-
-					bmp.SetPixel(x,y, Color.FromArgb(a,r,g,b));
-				}
-			}
+			Thread t = new Thread(ImageProcessor.invert);
+			t.Start(file);
 		}
     }
+	/// <summary>
+	/// inverts color scheme
+	/// </summary>
+	/// <param name="file">file</param>
+	public static void invert(object file)
+	{
+		string name = file.ToString();
+		int width;
+		int height;
+		string extension = "";
+		string filename = "";
+
+		Bitmap bmp = new Bitmap(name);
+		width = bmp.Width;
+		height = bmp.Height;
+		extension = name.Substring(name.IndexOf("."));
+		filename = name.Substring(name.IndexOf("/") + 1, name.Length - extension.Length);
+
+		for (int x = 0; x < width; ++x)
+		{
+			for ( int y = 0; y < height; ++y)
+			{
+				Color color = bmp.GetPixel(x,y);
+
+				int r = 255 - color.R;
+				int g = 255 - color.G;
+				int b = 255 - color.B;
+				int a = color.A;
+
+				bmp.SetPixel(x,y, Color.FromArgb(a,r,g,b));
+			}
+		}
+	}
 }
