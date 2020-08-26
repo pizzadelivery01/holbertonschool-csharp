5ea0c5918963eee4bb3d54e759fa81e99ed05f1d
jeremy bedolla <jjbedolla@gmail.com>
2020-08-26 13:35:09 -0400

    re adding

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class VectorMath
+{
+    /// <summary>
+    /// method that calculates length of a given vector
+    /// </summary>
+    /// <param name="vector">array containing vector</param>
+    /// <returns>returns the length of a given vector</returns>
+    public static double Magnitude(double[] vector)
+    {
+        double x = 0;
+
+        if (vector.Length < 2 || vector.Length > 3)
+        {
+          return -1;
+        }
+        foreach (double item in vector)
+        {
+            x += Math.Pow(x, 2);
+        }
+        return Math.Round(Math.Sqrt(x), 2);
+    }
+}
