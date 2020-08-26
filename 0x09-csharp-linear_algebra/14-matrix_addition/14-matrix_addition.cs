b0baf733c750cd6dcb3657637fb5791c91351c12
jeremy bedolla <jjbedolla@gmail.com>
2020-08-26 19:52:44 -0400

    adding

---
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class MatrixMath
+{
+	/// <summary>
+	/// adding matix
+	/// </summary>
+	/// <param name="matrix1"></param>
+	/// <param name="matrix2"></param>
+	/// <returns>matrix</returns>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        int len1 = matrix1.GetLength(1);
+        int len2 = matrix2.GetLength(1);
+        double[,] matrix_ret = new double[len1, len1];
+        if ((len1 == 2 && len2 == 2) || (len1 == 3 && len2 == 3))
+        {
+            for (int i = 0; i < len1; i++)
+            {
+                for (int j = 0; j < len1; j++)
+                {
+                    matrix_ret[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+            return matrix_ret;
+        }
+        return -1;
+    }
+}
