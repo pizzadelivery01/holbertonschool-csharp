46c6a216662d7e3d3b03d083da7f4ceb73d4a24a
jeremy bedolla <jjbedolla@gmail.com>
2020-08-26 19:45:27 -0400

    adding

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class VectorMath
+{
+	/// <summary>
+	/// multiplies two vectors and returns the resulting vector
+	/// </summary>
+	/// <param name="vector1">vector</param>
+	/// <param name="vector2">vector</param>
+	/// <returns>vector</returns>
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        double[] x = new double[vector.Length];
+
+        if (vector1.Length != vector2.Length || vector1.Length < 2 || vector1.Length > 3)
+        {
+            return -1;
+        }
+
+        for (int i = 0; i < vector1.Length; i++)
+        {
+            x += vector1[i] * vector2[i];
+        }
+
+        return Math.Round(x, 2);
+    }
+}
