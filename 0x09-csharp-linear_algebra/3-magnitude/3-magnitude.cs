d397336dbbfba0bff8f22973cde20baaa8e47070
jeremy bedolla <jjbedolla@gmail.com>
2020-08-26 13:25:17 -0400

    exact 2 or 3 if

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -10,12 +10,12 @@ class VectorMath
         public static double Magnitude(double[] vector)
         {
             double x = 0;
-            if (vector.Length <= 3 && vector.Length > 1)
+            if (vector.Length == 3 || vector.Length == 2)
             {
             	foreach (double item in vector)
                 	x += Math.Pow(x, 2);
-            	return (Math.Round(Math.Sqrt(x), 2));
+            	return Math.Round(Math.Sqrt(x), 2);
         	}
-			return (-1);
+			return -1;
         }
     }
