1689f93f5ce631dc938e91f8fec37f0ccb063947
jeremy bedolla <jjbedolla@gmail.com>
2020-08-26 20:57:56 -0400

    adding

---
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+class MatrixMath
+{
+    /// <summary>
+	/// Gets Dererminant
+	/// </summary>
+	/// <param name="matrix">matrix</param>
+	/// <returns>double</returns>
+    public static double Determinant(double[,] matrix)
+    {
+        double[,] err = new double[,] {{-1}};
+        double det = 0;
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+        {
+            det = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+        }
+        else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+        {
+            det = Math.Round(
+                (matrix[0, 0] * matrix[1, 1] * matrix[2, 2]) +
+                (matrix[1, 0] * matrix[2, 1] * matrix[0, 2]) +
+                (matrix[2, 0] * matrix[0, 1] * matrix[1, 2]) -
+                (matrix[0, 0] * matrix[2, 1] * matrix[1, 2]) -
+                (matrix[2, 0] * matrix[1, 1] * matrix[0, 2]) -
+                (matrix[1, 0] * matrix[0, 1] * matrix[2, 2]), 2);
+        }
+        else
+        {
+            return (-1);
+        }
+        return (det);
+    }
+}
