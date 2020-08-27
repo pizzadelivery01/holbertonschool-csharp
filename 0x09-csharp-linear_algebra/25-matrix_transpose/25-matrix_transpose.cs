955f050e71ae204af632cfa400eba2a5f02f67c5
jeremy bedolla <jjbedolla@gmail.com>
2020-08-26 20:54:29 -0400

    adding

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class MatrixMath
+{
+	/// <summary>
+	/// transpose matrix
+	/// </summary>
+	/// <param name="matrix">matrix</param>
+	/// <returns>matrix</returns>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        double[,] err = new double[,] {{-1}};
+        double[,] x = new double[matrix.GetLength(1), matrix.GetLength(0)];
+
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(0); j++)
+            {
+                x[i, j] = matrix[j, i];
+            }
+        }
+        return (x);
+    }
+}
