e848070ef68dc1f71853a95748e6594bc8aceb57
jeremy bedolla <jjbedolla@gmail.com>
2020-08-26 21:32:27 -0400

    refactor

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -8,25 +8,30 @@ class MatrixMath
 	/// </summary>
 	/// <param name="matrix">matrix</param>
 	/// <returns>inverse matrix</returns>
-    public static double[,] Inverse2D(double[,] matrix)
+	public static double[,] Inverse2D(double[,] matrix)
     {
-        double[,] err = new double[,] {{-1}};
-        double det;
+		double[,] err = new double[,]{{-1}};
 
-        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+        if (matrix == null || matrix.GetLength(0) != 2 || matrix.GetLength(0) != matrix.GetLength(1))
+            return (err);
+
+        double temp = matrix[1, 1];
+
+        matrix[1, 1] = matrix[0, 0];
+        matrix[0, 0] = temp;
+        matrix[0, 1] *= -1;
+        matrix[1, 0] *= -1;
+
+        double det = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+
+        if (det != 0)
         {
-            det = ((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]));
-            if (det == 0)
-                return (err);
-			else
-			{
-            	double[,] x = new double[,] {
-                	{(1 / det) * matrix[1, 1], (1 / det) * -matrix[0, 1]},
-                	{(1 / det) * -matrix[1, 0], (1 / det) * matrix[0, 0]}
-            	};
-            	return (x);
-			}
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j] = Math.Round(matrix[i, j] / det, 2);
+
+            return (matrix);
         }
-        return (err);
+        return (new double[,]{{-1}});
     }
 }
