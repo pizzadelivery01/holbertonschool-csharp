03160fea7f16ca53b2409332dfad2484f77136af
jeremy bedolla <jjbedolla@gmail.com>
2020-10-27 14:47:28 -0400

    adding task 1

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public interface IInteractive
+{
+	void Interact();
+}
+
+public interface IBreakable
+{
+	int durability {get; set;}
+	void Break();
+}
+
+public interface ICollectable
+{
+	bool isCollected {get; set;}
+
+	void Collect();
+}
+
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+
+}
