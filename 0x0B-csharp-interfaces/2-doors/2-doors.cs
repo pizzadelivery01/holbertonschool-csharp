48274194edd30067f982ea0a989ef9c83bcbf3b4
jeremy bedolla <jjbedolla@gmail.com>
2020-10-27 16:08:20 -0400

    adding task 2

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,73 @@
+﻿using System;
+/// <summary>
+/// interface for interactive
+/// </summary>
+interface IInteractive
+{
+	/// <summary>
+	/// interact
+	/// </summary>
+	void Interact();
+}
+/// <summary>
+/// interface for breakable
+/// </summary>
+interface IBreakable
+{
+	/// <summary>
+	/// durability
+	/// </summary>
+	/// <value>int</value>
+	int durability {get; set;}
+	/// <summary>
+	/// method to break 
+	/// </summary>
+	void Break();
+}
+/// <summary>
+/// interface for collectable
+/// </summary>
+interface ICollectable
+{
+	/// <summary>
+	/// is item collected
+	/// </summary>
+	/// <value>bool</value>
+	bool isCollected {get; set;}
+	/// <summary>
+	/// method to collect
+	/// </summary>
+	void Collect();
+}
+
+
+/// <summary>
+/// Base class abstract
+/// </summary>
+public abstract class Base
+{
+	/// <summary>
+	/// Name
+	/// </summary>
+	/// <value>string</value>
+	private string name {get; set;}
+	/// <summary>
+	/// overridee ToString method
+	/// </summary>
+	/// <returns>string</returns>
+	public override string ToString()
+	{
+		return  (name + " is a " + this.GetType().ToString());
+	}
+}
+class Door : Base, IInteractive
+{
+	public Door(string doorname = "Door")
+	{
+		name = doorname;
+	}
+	public void Interact()
+	{
+		Console.WriteLine($"You try to open the {name}. It's locked.");
+	}
+}
