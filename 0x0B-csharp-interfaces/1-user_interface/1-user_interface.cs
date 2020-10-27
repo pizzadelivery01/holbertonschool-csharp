06e33b4c8c38bbf43cbb08bda4feca3dc9ebf53f
jeremy bedolla <jjbedolla@gmail.com>
2020-10-27 14:55:56 -0400

    docs

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,32 +1,62 @@
 ﻿using System;
-
+/// <summary>
+/// interface for interactive
+/// </summary>
 public interface IInteractive
 {
+	/// <summary>
+	/// interact
+	/// </summary>
 	void Interact();
 }
-
+/// <summary>
+/// interface for breakable
+/// </summary>
 public interface IBreakable
 {
+	/// <summary>
+	/// durability
+	/// </summary>
+	/// <value>int</value>
 	int durability {get; set;}
+	/// <summary>
+	/// method to break 
+	/// </summary>
 	void Break();
 }
-
+/// <summary>
+/// interface for collectable
+/// </summary>
 public interface ICollectable
 {
+	/// <summary>
+	/// is item collected
+	/// </summary>
+	/// <value>bool</value>
 	bool isCollected {get; set;}
-
+	/// <summary>
+	/// method to collect
+	/// </summary>
 	void Collect();
 }
-
+/// <summary>
+/// Test Object
+/// </summary>
 class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
 	public int durability {get; set;}
 	public bool isCollected {get; set;}
 	public string name {get; set;}
+	/// <summary>
+	/// interact
+	/// </summary>
 	public void Interact()
 	{
 		
 	}
+	/// <summary>
+	/// collect
+	/// </summary>
 	public void Collect()
 	{
 
