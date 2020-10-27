0155f5f12f7622caf9d77830853f52bda1d528ea
jeremy bedolla <jjbedolla@gmail.com>
2020-10-27 14:30:09 -0400

    adding task 0

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+/// <summary>
+/// Base class abstract
+/// </summary>
+public abstract class Base
+{
+	/// <summary>
+	/// Name
+	/// </summary>
+	/// <value>string</value>
+	public string Name {get; set;}
+	/// <summary>
+	/// overridee ToString method
+	/// </summary>
+	/// <returns>string</returns>
+	public override string ToString()
+	{
+		return  Name + " is a " + Name.GetType();
+	}
+}
